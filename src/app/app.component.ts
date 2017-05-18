4c3ea3f76460061a5fd7ec242063b9563dd54e73
Seth Davenport <seth@rangle.io>
2017-05-18 10:20:43 +0200

    test out redux-batch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NgRedux } from '@angular-redux/store';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/do';
+import { Action } from 'redux';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  count$: Observable<number>;
+  private batch: Action[] = [];
+
+  constructor(private store: NgRedux<number>) {
+    this.count$ = store
+      .select()
+      .do(console.log.bind(console, 'SELECT CALLED WITH VALUE'));
+  }
+
+  singleDispatch() {
+    this.store.dispatch({ type: 'INCREMENT' });
+  }
+
+  addToBatch() {
+    this.batch.push({ type: 'INCREMENT' });
+  }
+
+  batchDispatch() {
+    this.store.dispatch<any>(this.batch);
+    this.batch = [];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { NgRedux, NgReduxModule } from '@angular-redux/store';
+import { reduxBatch } from '@manaflair/redux-batch';
+import { createLogger } from 'redux-logger';
 import { AppComponent } from './app.component';
+
+const counterReducer = (state = 0, { type }) => {
+  console.log('REDUCER CALLED WITH VALUE', state);
+  return type === 'INCREMENT' ? state + 1 : state;
+}
 
 @NgModule({
   declarations: [
@@ -12,9 +19,14 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgReduxModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(private store: NgRedux<number>) {
+    store.configureStore(counterReducer, 0, [], [reduxBatch]);
+  }
+}
